610927a99dcf6c05901124ffb57e2f0abd85be56
Carlos Saucedo <carlos@cgsphoto.com>
2020-11-29 23:23:04 -0500

    Changing sqlite data.

--- DodoPlanner/DodoPlanner/wwwroot/data/gen_db.sql
+++ DodoPlanner/DodoPlanner/wwwroot/data/gen_db.sql
@@ -1,44 +1,43 @@
 
-CREATE TABLE Category(
-      catID CHAR(36),
-      catName VARCHAR(25),
-      color VARCHAR(25),
-      PRIMARY KEY(catID)
+CREATE TABLE Categories(
+  catID CHAR(36),
+  catName VARCHAR(25),
+  color VARCHAR(25),
+  PRIMARY KEY(catID)
 );
-CREATE TABLE List(
-      listName VARCHAR(25),
-      listColor VARCHAR(25),
-      listID CHAR(36),
-      catID CHAR(36),
-      PRIMARY KEY(listID, catID),
-      FOREIGN KEY(catID) REFERENCES Category
+CREATE TABLE Lists(
+  listName VARCHAR(25),
+  listID CHAR(36),
+  catID CHAR(36),
+  PRIMARY KEY(listID, catID),
+  FOREIGN KEY(catID) REFERENCES Categories
 );
-CREATE TABLE User(
-      username VARCHAR(25),
-      password VARCHAR(50),
-      PRIMARY KEY(username)
+CREATE TABLE Users(
+  username VARCHAR(25),
+  password VARCHAR(50),
+  PRIMARY KEY(username)
 );
-CREATE TABLE Item(
-      itemID CHAR(36),
-      itemColor VARCHAR(25),
-      title VARCHAR(25),
-      description VARCHAR(140),
-      dueDate DATE,
-      listID CHAR(36),
-      PRIMARY KEY(itemID, listID),
-      FOREIGN KEY(listID) REFERENCES List
+CREATE TABLE Items(
+  itemID CHAR(36),
+  title VARCHAR(25),
+  description VARCHAR(140),
+  dueDate DATE,
+  completed INTEGER,
+  listID CHAR(36),
+  PRIMARY KEY(itemID, listID),
+  FOREIGN KEY(listID) REFERENCES Lists
 );
 CREATE TABLE Is_In(
-      itemID CHAR(36),
-      listID CHAR(36),
-      PRIMARY KEY (itemID, listID),
-      FOREIGN KEY (itemID) REFERENCES Item,
-      FOREIGN KEY (listID) REFERENCES List
+  itemID CHAR(36),
+  listID CHAR(36),
+  PRIMARY KEY (itemID, listID),
+  FOREIGN KEY (itemID) REFERENCES Items,
+  FOREIGN KEY (listID) REFERENCES Lists
 );
 CREATE TABLE Can_View(
-      username VARCHAR(25),
-      listID CHAR(36),
-      PRIMARY KEY (username, listID),
-      FOREIGN KEY (username) REFERENCES User,
-      FOREIGN KEY (listID) REFERENCES List
+  username VARCHAR(25),
+  listID CHAR(36),
+  PRIMARY KEY (username, listID),
+  FOREIGN KEY (username) REFERENCES Users,
+  FOREIGN KEY (listID) REFERENCES Lists
 );
---
+++ DodoPlanner/DodoPlanner/wwwroot/data/insert_sample_data.sql
@@ -0,0 +1,56 @@
+INSERT INTO Categories VALUES(
+  "026b7678-f314-40ab-ba30-c6214d2e0d05",
+  "School",
+  "blue"
+);
+INSERT INTO Lists VALUES(
+  "Algos",
+  "acbab712-15ec-4935-bfba-45cec47023a6",
+  "026b7678-f314-40ab-ba30-c6214d2e0d05"
+);
+INSERT INTO Items VALUES(
+  "9a3eb130-8053-449e-b511-26eabc3797d1",
+  "Work on hw 4",
+  "Do my hw 4 homework. Bryan forgot to add a field for this.",
+  "2020-11-27T00:00:00",
+  0,
+  "acbab712-15ec-4935-bfba-45cec47023a6"
+);
+INSERT INTO Items VALUES(
+  "7cbe92df-2336-4e3c-a762-6b050c61be64",
+  "Hw4 due",
+  "Hw4 due. Bryan forgot to add a field for this.",
+  "2020-12-02T00:00:00",
+  0,
+  "acbab712-15ec-4935-bfba-45cec47023a6"
+);
+INSERT INTO Items VALUES(
+  "bd52c324-c9b0-4177-9817-f47ce60bf726",
+  "Study for final",
+  "Study for final. Bryan forgot to add a field for this.",
+  "2020-12-05T00:00:00",
+  0,
+  "acbab712-15ec-4935-bfba-45cec47023a6"
+);
+
+INSERT INTO Items VALUES(
+  "97a1e599-c760-4c8d-be34-9ad6232693bd",
+  "study more",
+  "study more. Bryan forgot to add a field for this.",
+  "2020-12-06T00:00:00",
+  0,
+  "acbab712-15ec-4935-bfba-45cec47023a6"
+);
+INSERT INTO Lists VALUES(
+  "new list5",
+  "92e22699-0f33-40b5-ac84-c3170b02424d",
+  "026b7678-f314-40ab-ba30-c6214d2e0d05"
+);
+INSERT INTO Items VALUES(
+  "53ab6d79-5eb6-40ca-a810-8bab74cbe483",
+  "new task",
+  "new task description",
+  "2020-11-27T00:00:00",
+  0,
+  "92e22699-0f33-40b5-ac84-c3170b02424d"
+);
